57c4ffa59ad43147ac5b87335aed9eb7099b9770
Paschke Max (DGS-EC/EAT4) <maxotto.paschke@de.bosch.com>
2021-06-15 14:16:21 +0200

    Examples added and base implementation for SimEnvironment

--- Race/Simulation/SimulationEnvironment.cpp
+++ Race/Simulation/SimulationEnvironment.cpp
@@ -1,1 +1,34 @@
 #include "SimulationEnvironment.h"
+
+using namespace Simulation;
+
+SimulationEnvironment* ExampleSimEnvironment()
+{
+	SimulationEnvironment* result = new SimulationEnvironment();
+	result->setAirpressure(1013 * MBAR2PASCAL);
+	result->setAirtemperatureCelsius(22);
+	result->setWinddirection(0);
+	result->setWindspeed(10);
+
+	return result;
+}
+
+void Simulation::SimulationEnvironment::setAirtemperatureCelsius(double Airtemperature)
+{
+	this->Airtemperature = Airtemperature;
+}
+
+void Simulation::SimulationEnvironment::setAirpressure(double Airpressure)
+{
+	this->Airpressure = Airpressure;
+}
+
+void Simulation::SimulationEnvironment::setWindspeed(double Windspeed)
+{
+	this->Windspeed = Windspeed;
+}
+
+void Simulation::SimulationEnvironment::setWinddirection(double Winddirection)
+{
+	this->Winddirection = Winddirection;
+}
--- Race/Simulation/SimulationEnvironment.h
+++ Race/Simulation/SimulationEnvironment.h
@@ -9,6 +9,11 @@ namespace Simulation
 		double calcWindSpeed(double VehicleDirection);
 		double calcRoadResistance();
 
+		void setAirtemperatureCelsius(double Airtemperature);
+		void setAirpressure(double Airpressure);
+		void setWindspeed(double Windspeed);
+		void setWinddirection(double Winddirection);
+
 	private:
 		double Airtemperature;
 		double Airpressure;
@@ -18,4 +23,13 @@ namespace Simulation
 
 	static double GRAVITATIONALCONSTANT = 9.81;
 	static double GASCONSTANT = 287.0529;
+
+	const double RPM2HZ = 1 / 60.0;
+	const double HZ2RPM = 60;
+
+	const double MS2KMH = 3.6;
+	const double KMH2MS = 1 / 3.6;
+
+	const double MBAR2PASCAL = 100;
+	const double PASCAL2MBAR = 1 / 100.0;
 }
