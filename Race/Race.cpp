1028836b5571db09bd4964c79f755550b575f8b7
s1kloewe <Konstantin.Loewen@de.bosch.com>
2021-06-19 12:01:07 +0200

    JSON Implemented (Vector is complete)

--- Race/Race.cpp
+++ Race/Race.cpp
@@ -1,6 +1,9 @@
 // Race.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
 //
 
+
+
+#include<stdio.h>
 #include "Race.h"
 #include "OpenStreetMap.h"
 
