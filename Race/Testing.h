86ecb3663836788e6bb85f1f5689b6a5b1b1723a
Max Paschke <maxotto.paschke@de.bosch.com>
2021-06-24 21:29:54 +0200

    Fixed circular reference introduced by testing.h

--- Race/Testing.h
+++ Race/Testing.h
@@ -1,8 +1,10 @@
 #pragma once
+#ifndef TESTINGDEF
+#define TESTINGDEF
+
 using namespace std;
 #include <string>
 #include "race.h"
-
 
 class Testing
 {
@@ -15,9 +17,11 @@ public:
 class ErrorLog
 {
 public:
-	vector<Testing> Testvektor=vector<Testing>();
+	vector<Testing> Testvektor = vector<Testing>();
 	void TestSollfahrtbestimmung(vector<node>& nodes);
 	void TestDatenAufbereitung(vector<node>& nodes);
 	void TestDatenbeschaffung(vector<node>& nodes);
 	void TestFahrphysik(vector<node>& nodes);
 };
+
+#endif
